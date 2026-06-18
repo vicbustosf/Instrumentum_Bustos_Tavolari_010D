2a1e584047ff72177d3018c7e536a44bb471e78d
vicbustosf <vic.bustosf@duocuc.cl>
2026-06-18 14:05:17 -0400

    Agregar microservicio autentificación

--- Instrumentum/service-auth/src/test/java/cl/instrumentum/service_auth/ServiceAuthApplicationTests.java
+++ Instrumentum/service-auth/src/test/java/cl/instrumentum/service_auth/ServiceAuthApplicationTests.java
@@ -1,10 +1,10 @@
-package com.instrumentum.service_usuario;
+package cl.instrumentum.service_auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServiceUsuarioApplicationTests {
+class ServiceAuthApplicationTests {
 
 	@Test
 	void contextLoads() {
