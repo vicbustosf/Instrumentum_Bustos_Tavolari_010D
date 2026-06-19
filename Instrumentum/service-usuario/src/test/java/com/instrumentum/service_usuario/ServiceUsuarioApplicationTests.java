90f23bbefb7670fce308f59570e757e1a2252f27
vicbustosf <vic.bustosf@duocuc.cl>
2026-06-19 18:16:44 -0400

    Preparar un detalle

---
+++ Instrumentum/service-usuario/src/test/java/com/instrumentum/service_usuario/ServiceUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instrumentum.service_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
